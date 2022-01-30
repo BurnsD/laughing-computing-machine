349d09a3875f4bbbc1863a33240043a2bfe44258
BurnsD <Dustinburnsactech@gmail.com>
2022-01-30 12:01:49 -0600

    intern html start

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -3,8 +3,19 @@ const generatePage = function (team) {
     // Intern 
     const genereateIntern = function (intern) {
         return `
-        
-        
+        <div>
+        <div>
+            <div class="header">
+                <h3 class="name">${intern.name}</h3>
+                <h4 class="role">Intern</h4>
+            </div>
+            <div class="body">
+                <p class="id">ID: ${intern.id}</p>
+                <p class="email">Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
+                <p class="school"> SChool: ${intern.school}}</p>
+            </div>
+        </div>
+        </div>
         `
     }
     // Engineer
