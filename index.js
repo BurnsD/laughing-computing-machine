285028454b8060c658c022068953feefd0934e11
BurnsD <Dustinburnsactech@gmail.com>
2022-01-30 00:21:27 -0600

    revising input prompts.

--- index.js
+++ index.js
@@ -14,120 +14,159 @@ const path = require('path');
 // Employee Array
 const employeeArray = [];
 
-const decideRole = () => {
+const addTeamMember = () => {
     inquirer
-        .prompt({
+        .prompt([{
           type: 'list',
           message: 'What role does this person take?',
           name: 'action',
           choices: ['Intern', 'Engineer', 'Manager',]
-        })
+        }
+    ]).then(choice => {
+        switch (choice.employeeType) {
+            case 'Intern':
+                addIntern();
+                break;
+            case 'Engineer':
+                addEngineer();
+                break;
+            case 'Manager':
+                addManager();
+            default:
+                fs.writeFile()
+        }
+    })
+}
+
+    // Intern
+    const addIntern = () => {
+        console.log("Enter Intern's Information")
+        // Questions for Intern Input
+        inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'internName',
+            message: 'Intern Name',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    return 'Please enter a name!';
+                }
+            }
+        }, {
+                type: 'input',
+                name: 'id',
+                message: 'ID Number',
+                validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    return 'Please enter ID!';
+                }
+            }
+        }, {
+                type: 'input',
+                name: 'email',
+                message: 'Email Address',
+                validate: emailInput => {
+                 if (emailInput) {
+                     return true;
+                } else {
+                     return 'Please enter e-mail!';
+                }
+            }
+        }, {
+                type: 'input',
+                name: 'school',
+                message: 'Which school does this intern attend?',
+                validate: schoolInput => {
+                if (schoolInput) {
+                     return true;
+                } else {
+                    return 'Please enter school name!';
+                }
+            }
+        },
+    ]).then(answers => {
+        console.log(answers);
+        const intern = new Intern(
+            answers.internName,
+            answers.id, 
+            answers.email, 
+            answers.school)
+        employeeArray.push(intern);
+        addTeamMember();
+    })
+}
+    // End Of Intern
+
+        // Engineer
+    const addEngineer = () => {
+    console.log("Enter Engineer's Information")
+            // Questions for Engineer Input
+        inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'engineerName',
+            message: 'Engineers Name',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+            } else {
+                return 'Please enter a name!';
+            }
+        }
+    }, {
+            type: 'input',
+            name: 'id',
+            message: 'ID Number',
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+            } else {
+                return 'Please enter ID!';
+            }
+        }
+    }, {
+            type: 'input',
+            name: 'email',
+            message: 'Email Address',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+            } else {
+                return 'Please enter e-mail!';
+            }
+        }
+    }, {
+            type: 'input',
+            name: 'github',
+            message: 'Enter GitHub Username.',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    return 'Please enter GitHub Username!';
+                }
+            }
+        },
+    ]).then(answers => {
+        console.log(answers);
+        const engineer = new Engineer(
+            answers.engineerName,
+            answers.id, 
+            answers.email, 
+            answers.github)
+        employeeArray.push(engineer);
+        addTeamMember();
+    })
+}
+    //End of Engineer
+
+        
+        // Manager
         .then(({ action }) => {
-            if (action === 'Intern'){
-                console.log("Enter Intern's Information")
-                // Questions for Intern Input
-                inquirer.prompt ([
-                    {
-                        type: 'input',
-                        name: 'internName',
-                        message: 'Intern Name',
-                        validate: nameInput => {
-                            if (nameInput) {
-                                return true;
-                            } else {
-                                return 'Please enter a name!';
-                            }
-                        }
-                    }, {
-                        type: 'input',
-                        name: 'id',
-                        message: 'ID Number',
-                        validate: idInput => {
-                            if (idInput) {
-                                return true;
-                            } else {
-                                return 'Please enter ID!';
-                            }
-                        }
-                    }, {
-                        type: 'input',
-                        name: 'email',
-                        message: 'Email Address',
-                        validate: emailInput => {
-                            if (emailInput) {
-                                return true;
-                            } else {
-                                return 'Please enter e-mail!';
-                            }
-                        }
-                    }, {
-                        type: 'input',
-                        name: 'school',
-                        message: 'Which school does this intern attend?',
-                        validate: schoolInput => {
-                            if (schoolInput) {
-                                return true;
-                            } else {
-                                return 'Please enter school name!';
-                            }
-                        }
-                    },
-                ]);
-            }
-        }).then(({ action }) => {
-            if (action === 'Engineer'){
-                
-                console.log("Enter Engineer's Information")
-                // Questions for Engineer Input
-                inquirer.prompt ([
-                    {
-                        type: 'input',
-                        name: 'engineerName',
-                        message: 'Engineers Name',
-                        validate: nameInput => {
-                            if (nameInput) {
-                                return true;
-                            } else {
-                                return 'Please enter a name!';
-                            }
-                        }
-                    }, {
-                        type: 'input',
-                        name: 'id',
-                        message: 'ID Number',
-                        validate: idInput => {
-                            if (idInput) {
-                                return true;
-                            } else {
-                                return 'Please enter ID!';
-                            }
-                        }
-                    }, {
-                        type: 'input',
-                        name: 'email',
-                        message: 'Email Address',
-                        validate: emailInput => {
-                            if (emailInput) {
-                                return true;
-                            } else {
-                                return 'Please enter e-mail!';
-                            }
-                        }
-                    }, {
-                        type: 'input',
-                        name: 'github',
-                        message: 'Enter GitHub Username.',
-                        validate: githubInput => {
-                            if (githubInput) {
-                                return true;
-                            } else {
-                                return 'Please enter GitHub Username!';
-                            }
-                        }
-                    },
-                ])
-            }
-        }).then(({ action }) => {
             if (action === 'Manager') {
                 console.log("Enter Manager's Information")
                 // Questions for Manager Input
@@ -180,4 +219,6 @@ const decideRole = () => {
                 ])
             }
         })
-}
+        // End of Manager
+
+
