231816f67939a9fb93db6d75b59e6a0b9d8c3d77
BurnsD <Dustinburnsactech@gmail.com>
2022-01-30 11:47:29 -0600

    added some functions to generateHTML

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,11 +1,26 @@
 // Function for Generating HTML
 const generatePage = function (team) {
     // Intern 
-
+    const genereateIntern = function (intern) {
+        return `
+        
+        
+        `
+    }
     // Engineer
-
+    const genereateEngineer = function (engineer) {
+        return `
+        
+        
+        `
+    }
     // Manager
-
+    const genereateManager = function (manager) {
+        return `
+        
+        
+        `
+    }
     // Push to Roles to Page
 
     // Return
